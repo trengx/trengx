398dbd6c992ac2ba035af89983da322b98ea0d1d
Yong-Jun <jun@trengx.com>
2023-06-19 01:28:06 -0700

    add edge working!

--- trengx/engine/add_node/networkx_add_node.py
+++ trengx/engine/add_node/networkx_add_node.py
@@ -2,4 +2,12 @@ from .add_node import AddNode
 
 class NetworkXAddNode(AddNode):
     def add_to_graph(self):
+        if not isinstance(self.label, str):
+            raise TypeError('label must be a string')
+        if not isinstance(self.id, str):
+            raise TypeError('id must be a string')
+        if not isinstance(self.name, str):
+            raise TypeError('name must be a string')
+        if not isinstance(self.grad, float):
+            raise TypeError('grad must be a float')
         self.driver.add_node(self.id, label=self.label, name=self.name, value=self.value, grad=self.grad)
