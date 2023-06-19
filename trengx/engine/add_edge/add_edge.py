df5484e16aa3a72020bbaa53148b360d6f05543a
trengx <128947466+trengx@users.noreply.github.com>
2023-06-19 01:30:03 -0700

    Generic add edge (#77)
    * initialized
    
    * forward propagation working!
    
    * apoc.path.expand with total path found initially
    
    * apoc.path.subgraphAll  working
    
    * output updated for forward_propagation_testing.ipynb
    
    * getNameByUuid working!
    
    * getNameByUuid working!
    
    * forward progagation working!
    
    * backpropagation working!
    
    * insulin subsystem
    
    * simulation working!
    
    * Gsub plotting working
    
    * D varies over time
    
    * Refactored code
    
    * PID control working!
    
    * Kp Ki Kd parameters tuned
    
    * Type1 elementary operation unit working!
    
    * engine.py added
    
    * Cleaned diabetes_simulator.ipynb
    
    * Neuron working!
    
    * Simple neural network working!
    
    * updated_nodes version
    
    * forward propagation updated nodes version working!
    
    * unittest added
    
    * tests for operations added
    
    * unittest for operations
    
    * draw operation type 1 and 2 created
    
    * deleted old operation files
    
    * created add_node.py
    
    * created add_edge.py
    
    * generic add node working
    
    * generic add node working
    
    * deleted old add_node.py
    
    * Refactored add_node
    
    * Added input type check
    
    * add edge working!

--- trengx/engine/add_edge/add_edge.py
+++ trengx/engine/add_edge/add_edge.py
@@ -1,13 +1,13 @@
 import uuid
 
-class AddNode:
-    def __init__(self, driver, id=None, label=None, name=None, value=None, grad=None):
+class AddEdge:
+    def __init__(self, driver, node1, node2, id=None, label=None, second_operand=None):
         self.driver = driver
+        self.node1 = node1
+        self.node2 = node2
         self.id = self._generate_unique_id(id)
         self.label = label
-        self.name = name
-        self.value = value
-        self.grad = grad
+        self.second_operand = second_operand
         self.add_to_graph()
 
     def _generate_unique_id(self, id):
@@ -19,4 +19,8 @@ class AddNode:
         raise NotImplementedError("add_to_graph method must be implemented in a subclass.")
     
     def __str__(self):
-        return f"Node(id={self.id}, label={self.label}, name={self.name}, value={self.value}, grad={self.grad})"
+        return f"Edge(id={self.id}, node1={self.node1}, node2={self.node2}, label={self.label}, second_operand={self.second_operand})"
+
+
+
+
--- trengx/engine/add_node/__init__.py
+++ trengx/engine/add_node/__init__.py
@@ -1,2 +1,1 @@
 from .node import node
-
